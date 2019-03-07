1b29b6fcb608e65dd6a0d67281be8fd560c1e477
Afonso Delgado <afonso.delgado@hotmail.com>
2019-03-07 09:57:53 -0300

    5 aula

--- 0x01-first_try/processer.py
+++ 0x01-first_try/processer.py
@@ -1,24 +1,45 @@
 import cv2
 import numpy as np
-from skimage.util import random_noise, img_as_ubyte
+#from skimage.util import random_noise, img_as_ubyte
 from keras.applications import imagenet_utils
 
 def processer(frame, frame_number, model, preprocess):
-    #do the processing
+    frame = gray_converter(frame)
     
+    pyrd1 = cv2.pyrDown(frame)
+    
+    pyrd2 = cv2.pyrDown(pyrd1)
    
-    processed = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    pyru1 = cv2.pyrUp(frame)
+    pyru2 = cv2.pyrUp(pyru1)
+    add_hist(frame)
+    add_hist(pyrd1)
+    add_hist(pyrd2)
+    add_hist(pyru1)
+    add_hist(pyru2)
+    cv2.putText(pyrd1,'d1', (0,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+    cv2.putText(pyrd2,'d2', (0,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+    cv2.putText(pyru1,'u1', (0,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+    cv2.putText(pyru2,'u2', (0,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+    cv2.imshow('pyrd1',pyrd1)
+    cv2.imshow('pyrd2',pyrd2)
+    cv2.imshow('pyru1',pyru1)
+    cv2.imshow('pyru2',pyru2)
+    #do the processing
+    #processed = gray_converter(frame)
+    #processed = cv2.bilateralFilter(processed,9,100,100)
+    #processed = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     #cv2.imshow('framer',processed)
-    processed = do_the_learning(processed, model, preprocess)
-    processed = cv2.cvtColor(processed, cv2.COLOR_RGB2BGR)
-    #cv2.putText(processed,'PROCESSED IMAGE', (0,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-    cv2.imshow('frame',processed)
+    #processed = do_the_learning(processed, model, preprocess)
+    #processed = cv2.cvtColor(processed, cv2.COLOR_RGB2BGR)
+    cv2.putText(frame,'PROCESSED IMAGE', (0,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+    cv2.imshow('frame',frame)
 
 
 def add_hist(img):
     h = cv2.calcHist([img],[0],None,[256],[0,256])
     for i, bin_ in enumerate(h):
-        cv2.line(img,(i,img.shape[0]),(i,img.shape[0] - int(bin_*0.01)),(255,255,255))
+        cv2.line(img,(i,img.shape[0]),(i,img.shape[0] - int(bin_*10/img.shape[0])),(255,255,255))
     cv2.line(img,(257,img.shape[0]),(257,img.shape[0] - 50),(0,255,255))
     return h.reshape((-1)).T
 
